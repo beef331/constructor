2e7510957971e14739f11c6684bc1ff8435cb390
Jason <beefers331@gmail.com>
2021-03-01 00:24:24 -0700

    Removed redundancy

--- src/constructor/implements.nim
+++ src/constructor/implements.nim
@@ -1,10 +1,13 @@
-import std/[macros, sugar]
+import std/[macros, tables]
 
 {.experimental: "dynamicBindSym".}
 
+when (NimMajor, NimMinor, NimPatch) < (1, 5, 1):
+  static: error: "This module requires atleast Nim 1.5.1 with new concepts"
+
 macro implements*(ident: untyped, concepts: varargs[typed], body: untyped): untyped =
   let ident = ident($ident)
-  var implTypes: seq[NimNode]
+  var implTypes: Table[string, NimNode]
   for x in concepts:
     for x in x.getImpl[2][3]:
       if x.kind == nnkProcDef:
@@ -17,12 +20,13 @@ macro implements*(ident: untyped, concepts: varargs[typed], body: untyped): unty
             if $impl[0] == $procName and $impl[3][0] == $x[3][0]:
               for i, def in impl[3]:
                 if def.len != impl[3][i].len and def[^2] != impl[3][i][^2]:
+                  implTypes.del($impl[3][1][^2])
                   break getType
               let implName = ident($impl[3][1][^2])
-              implTypes.add quote do:
+              implTypes[$implName] = quote do:
                 `ident` of `implName`
   var conditions: seq[(Nimnode, NimNode)]
-  for implType in implTypes:
+  for implType in implTypes.values:
     var 
       body = body.copyNimTree
       t = implType[^1]
