b948d29e9261bc1b009e686173d547fbecb1112e
Jason <beefers331@gmail.com>
2022-01-15 18:44:41 -0700

    Fixed issues with weird ast proc definitions

--- src/constructor/constructor.nim
+++ src/constructor/constructor.nim
@@ -27,10 +27,17 @@ macro constr*(p: typed): untyped =
 
   for defs in result.params[1..^1]:
     extractIdentDef(defs)
-  if result[^1].kind == nnkSym: # Weird AST
-    result[^2] = newStmtList(nnkAsgn.newTree(ident"result", constrStmt))
+
+  if result[^2].kind != nnkEmpty and result[^1].kind == nnkSym:
+    # Handles weird ast where last node is `sym` and second last is `body`
+    result[^2] = newStmtList(nnkAsgn.newTree(result[^1], constrStmt), result[^2])
   else:
-    result[^1] = newStmtList(nnkAsgn.newTree(ident"result", constrStmt)):
-      result[^1]
-  if {p[^1].kind, p[^2].kind} == {nnkEmpty}:
-    result = newStmtList(p, result)
+    # Handles weird case where last node is `body` and second last is bracket expr
+    result[^1] =
+      if result[^1].kind != nnkEmpty:
+        newStmtList(nnkAsgn.newTree(ident"result", constrStmt), result[^1])
+      else:
+        nnkAsgn.newTree(ident"result", constrStmt)
+    if {p[^1].kind, p[^2].kind} == {nnkEmpty}:
+      # Is this a forward declare
+      result = newStmtList(p, result)
--- tests/tconstructor.nim
+++ tests/tconstructor.nim
@@ -13,3 +13,12 @@ proc init(T: typedesc[User], name: string, age: int): User {.constr.} =
 
 assert initUser("hello", 10) == User(name: "hello", lastOnline: 0f, age: 10)
 assert User.init("hello", 30) == User(name: "hello", lastOnline: 30f, age: 30)
+
+
+import sequtils
+type Field* = object
+  opts*: seq[(string, string)]
+  name*: string
+proc initField*(name = "", options: openarray[(string, string)]): Field {.constr.} =
+  result.opts = toSeq(options)
+assert initField("beef", {"Hi": "Mr. Beef"}) == Field(opts: @{"Hi": "Mr. Beef"}, name: "beef")
