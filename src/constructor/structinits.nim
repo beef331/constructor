f3364009f58cae81a6b2a47c7305b3aea6e5d433
Jason Beetham <beefers331@gmail.com>
2023-02-05 02:23:17 -0700

    Fixed #10, by using a global `paramCounter`, that is reset inside code

--- src/constructor/structinits.nim
+++ src/constructor/structinits.nim
@@ -1,8 +1,11 @@
 import std/[macros, genasts, strutils]
 
-macro unpack(args: varargs[typed], index: static int): untyped =
+
+var paramCounter {.compileTime.} = 0
+
+macro unpack(args: varargs[typed]): untyped =
   # Retrieve the value inside a varargs of 'index'
-  result = args[index]
+  result = args[paramCounter]
 
 macro init*(typ: typedesc[object], args: varargs[typed]): untyped =
   ## init constructor that uses order of args to assign to type's fields.
@@ -18,16 +21,15 @@ macro init*(typ: typedesc[object], args: varargs[typed]): untyped =
     unpackCall = newCall(bindSym"unpack")
   for arg in args:
     unpackCall.add arg
-  unpackCall.add i
 
-  result = genast(typ, args, i, unpackCall):
+  result = genast(typ, args, i, unpackCall, paramCounter = bindSym"paramCounter"):
     var res: typ
-    var i {.compileTime.} = 0
+    static: paramCounter = 0
     for name, field in res.fieldPairs: # Perhaps use disruptek's assume here
       when not compiles((let a: typeof(field) = unpackCall)):
-          {.error: "Field '$#' (position $#) is of type '$#', but got a value type of '$#'." % [name, $i, $typeof(field), $typeof(unpackCall)].}
+          {.error: "Field '$#' (position $#) is of type '$#', but got a value type of '$#'." % [name, $paramCounter, $typeof(field), $typeof(unpackCall)].}
       field = unpackCall
-      static: inc i
+      static: inc paramCounter
     res
 
 macro new*(typ: typedesc[ref object or object], args: varargs[typed]): untyped =
@@ -45,12 +47,12 @@ macro new*(typ: typedesc[ref object or object], args: varargs[typed]): untyped =
     unpackCall.add arg
   unpackCall.add i
 
-  result = genast(typ, args, i, unpackCall):
+  result = genast(typ, args, i, unpackCall, paramCounter = bindSym"paramCounter"):
     var res = system.new(typ)
-    var i {.compileTime.} = 0
+    static: paramCounter = 0
     for name, field in res[].fieldPairs: # Perhaps use disruptek's assume here
       when not compiles((let a: typeof(field) = unpackCall)):
-        {.error: "Field '$#' (position $#) is of type '$#', but got a value type of '$#'." % [name, $i, $typeof(field), $typeof(unpackCall)].}
+        {.error: "Field '$#' (position $#) is of type '$#', but got a value type of '$#'." % [name, $paramCounter, $typeof(field), $typeof(unpackCall)].}
       field = unpackCall
-      static: inc i
+      static: inc paramCounter
     res
--- tests/tstructinits.nim
+++ tests/tstructinits.nim
@@ -14,3 +14,19 @@ check MyType.new(10, 20, "hello")[] == (ref MyType)(x: 10, y: 20, z: "hello")[]
 check MyRef.new(10, 20, "Hello")[] == MyRef(x: 10, y: 20, z: "Hello")[]
 check MyGen[int].init(100) == MyGen[int](x: 100)
 check MyGen[int].new(100)[] == (ref MyGen[int])(x: 100)[]
+
+
+import std/[os, times]
+
+type FileInfo* = object
+  path*: string
+  size*: BiggestInt
+  exists*: bool
+
+type Image* = object
+  file*: FileInfo
+  dest*: string
+  date*: DateTime
+
+proc initImage*(file: FileInfo, dest: string, lastModified: Time): Image =
+  Image.init(default(FileInfo), "", default(DateTime))
