67a48b3df5b5ef4b40660472b58f7e287a727d5a
Jason Beetham <beefers331@gmail.com>
2023-03-17 20:09:07 -0600

    Added specific field assignment to structinits

--- src/constructor/structinits.nim
+++ src/constructor/structinits.nim
@@ -1,13 +1,11 @@
-import std/[macros, genasts, strutils]
+import std/[macros, genasts, strutils, intsets]
 
 
-var paramCounter {.compileTime.} = 0
-
-macro unpack(args: varargs[typed]): untyped =
+macro unpack(count: static int, args: varargs[typed]): untyped =
   # Retrieve the value inside a varargs of 'index'
-  result = args[paramCounter]
+  result = args[count]
 
-macro init*(typ: typedesc[object], args: varargs[typed]): untyped =
+macro init*(typ: typedesc[object], args: varargs[untyped]): untyped =
   ## init constructor that uses order of args to assign to type's fields.
   ## Presently only works for non object variants
   runnableExamples:
@@ -18,21 +16,57 @@ macro init*(typ: typedesc[object], args: varargs[typed]): untyped =
 
   let
     i = gensym(nskVar, "i")
-    unpackCall = newCall(bindSym"unpack")
-  for arg in args:
-    unpackCall.add arg
+    paramCounter = gensym(nskVar, "paramCounter")
+    unpackCall = newCall(bindSym"unpack", paramCounter)
+    res = genSym(nskVar, "result")
 
-  result = genast(typ, args, i, unpackCall, paramCounter = bindSym"paramCounter"):
+  var
+    addins = newStmtList()
+    lastVal = -1
+
+  for i, arg in args:
+    if arg.kind == nnkExprEqExpr:
+      let left =
+        if arg[0].kind == nnkBracketExpr:
+          let bracketExpr = arg[0].copyNimTree
+          bracketExpr[0] = nnkDotExpr.newTree(res, bracketExpr[0])
+          bracketExpr
+        else:
+          arg[0]
+
+      addins.add:
+        nnkAsgn.newTree(
+          left,
+          arg[1]
+        )
+
+      if lastVal < 0:
+        lastVal = i
+
+    else:
+      if lastVal >= 0:
+        error("Provided positional setter, following expression setters ambiguous what to do.", arg)
+      unpackCall.add arg
+
+  if addins.len == 0:
+    lastVal = args.len
+
+  result = genast(res, typ, args, i, unpackCall, addins, lastVal, paramCounter):
     var res: typ
+    var paramCounter {.compileTime, global.} = 0
     static: paramCounter = 0
     for name, field in res.fieldPairs: # Perhaps use disruptek's assume here
-      when not compiles((let a: typeof(field) = unpackCall)):
+      when paramCounter < lastVal:
+        when not compiles((let a: typeof(field) = unpackCall)):
           {.error: "Field '$#' (position $#) is of type '$#', but got a value type of '$#'." % [name, $paramCounter, $typeof(field), $typeof(unpackCall)].}
-      field = unpackCall
-      static: inc paramCounter
+        field = unpackCall
+        static: inc paramCounter
+
+
+    addins
     res
 
-macro new*(typ: typedesc[ref object or object], args: varargs[typed]): untyped =
+macro new*(typ: typedesc[ref object or object], args: varargs[untyped]): untyped =
   ## Same as init but heap allocates instead, accepts `ref object` or `object` making `object` into a `ref`.
   ## Presently only works for non object variants
   runnableExamples:
@@ -40,19 +74,59 @@ macro new*(typ: typedesc[ref object or object], args: varargs[typed]): untyped =
       x, y: int
       z: string
     assert MyType.new(10, 20, "hello")[] == (ref MyType)(x: 10, y: 20, z: "hello")[]
+
   let
     i = gensym(nskVar, "i")
-    unpackCall = newCall(bindSym"unpack")
-  for arg in args:
-    unpackCall.add arg
-  unpackCall.add i
+    paramCounter = gensym(nskVar, "paramCounter")
+    unpackCall = newCall(bindSym"unpack", paramCounter)
+    res = genSym(nskVar, "result")
 
-  result = genast(typ, args, i, unpackCall, paramCounter = bindSym"paramCounter"):
+  var
+    addins = newStmtList()
+    lastVal = -1
+
+  for i, arg in args:
+    if arg.kind == nnkExprEqExpr:
+      let left =
+        if arg[0].kind == nnkBracketExpr:
+          let bracketExpr = arg[0].copyNimTree
+          bracketExpr[0] = nnkDotExpr.newTree(res, bracketExpr[0])
+          bracketExpr
+        else:
+          arg[0]
+
+      addins.add:
+        nnkAsgn.newTree(
+          left,
+          arg[1]
+        )
+
+      if lastVal < 0:
+        lastVal = i
+
+    else:
+      if lastVal >= 0:
+        error("Provided positional setter, following expression setters ambiguous what to do.", arg)
+      unpackCall.add arg
+
+  if addins.len == 0:
+    lastVal = args.len
+
+  result = genast(res, typ, args, i, unpackCall, addins, lastVal, paramCounter):
     var res = system.new(typ)
+    var paramCounter {.compileTime, global.} = 0
     static: paramCounter = 0
     for name, field in res[].fieldPairs: # Perhaps use disruptek's assume here
-      when not compiles((let a: typeof(field) = unpackCall)):
-        {.error: "Field '$#' (position $#) is of type '$#', but got a value type of '$#'." % [name, $paramCounter, $typeof(field), $typeof(unpackCall)].}
-      field = unpackCall
-      static: inc paramCounter
+      when paramCounter < lastVal:
+        when not compiles((let a: typeof(field) = unpackCall)):
+          {.error: "Field '$#' (position $#) is of type '$#', but got a value type of '$#'." % [name, $paramCounter, $typeof(field), $typeof(unpackCall)].}
+        field = unpackCall
+        static: inc paramCounter
+
+
+    addins
     res
+
+
+
+
--- tests/tstructinits.nim
+++ tests/tstructinits.nim
@@ -8,6 +8,9 @@ type
   MyRef = ref MyType
   MyGen[T] = object
     x: int
+  MyArr = object
+    x, y: int
+    myArr: array[3, int]
 
 check MyType.init(10, 20, "hello") == MyType(x: 10, y: 20, z: "hello")
 check MyType.new(10, 20, "hello")[] == (ref MyType)(x: 10, y: 20, z: "hello")[]
@@ -15,8 +18,10 @@ check MyRef.new(10, 20, "Hello")[] == MyRef(x: 10, y: 20, z: "Hello")[]
 check MyGen[int].init(100) == MyGen[int](x: 100)
 check MyGen[int].new(100)[] == (ref MyGen[int])(x: 100)[]
 
+check MyArr.init(1, 2, myArr[0] = 3, myArr[1] = 4) == MyArr(x: 1, y: 2, myArr: [3, 4, 0])
 
-import std/[os, times]
+
+import std/[times]
 
 type FileInfo* = object
   path*: string
